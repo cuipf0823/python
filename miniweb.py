9c87d54bdd20c4cc8c9499f7a70bebb6bf289d89
cuipf1989 <570094028@qq.com>
2016-10-31 14:36:47 +0800

    flask_boorstrap

--- miniweb.py
+++ miniweb.py
@@ -4,8 +4,10 @@ from flask import Flask
 from flask import request
 from flask import make_response
 from flask import render_template
+from flask_bootstrap import Bootstrap
 
 app=Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 # index 试图函数
@@ -38,6 +40,25 @@ def response():
 @app.route('/login/<name>')
 def login(name):
     return render_template('login.html', name=name)
+
+
+# 使用flask-bootstrap
+@app.route('/check/<name>')
+def cheak(name):
+    return render_template('check.html', name=name)
+
+
+# 错误页面处理
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+# 有未处理的异常的时候
+@app.errorhandler(500)
+def internal_server_error(e):
+    return 'Sorry, 500server error!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
