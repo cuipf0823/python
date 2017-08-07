70f43d5265cca71711a43b635db1b3d1b6909abc
cuipf0823 <570094028@qq.com>
2017-08-07 22:42:28 +0800

    add edit profile

--- blog/app/main/forms.py
+++ blog/app/main/forms.py
@@ -2,10 +2,14 @@
 # coding=utf-8
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms import BooleanField
+from wtforms import SelectField
 from wtforms import SubmitField
 from wtforms import TextAreaField
-from wtforms.validators import Length
 from wtforms.validators import DataRequired
+from wtforms.validators import Length
+from wtforms.validators import Email
+from wtforms.validators import Regexp
 
 
 class NameForm(FlaskForm):
@@ -18,3 +22,21 @@ class EditProfileForm(FlaskForm):
     location = StringField('Location', validators=[Length(0, 128)])
     about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
+
+
+class EditProfileFormAdmin(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Length(1, 128), Email()])
+    username = StringField('Username', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                         'Username must hava only '
+                                                                                         'letters, numbers, '
+                                                                                         'dots or underscores')])
+    confirmed = BooleanField('Confirmed')
+    role = SelectField('Role', coerce=int)
+    name = StringField('Real Name', validators=[Length(0, 64)])
+    location = StringField('Location', validators=[Length(0, 128)])
+    about_me = TextAreaField('About me')
+    submit = SubmitField('Submit')
+
+    def __init__(self, *args, **kwargs):
+        super(EditProfileFormAdmin, self).__init__(*args, **kwargs)
+        # self.role.choices = [(role.id, role.name)]
